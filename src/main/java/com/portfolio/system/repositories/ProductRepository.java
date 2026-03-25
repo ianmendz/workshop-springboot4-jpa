87a8cf726949ea2527f69ff37e536f29d7eebda0
ianmendz <ian.mendzz@gmail.com>
2026-03-25 19:23:19 -0300

    Product

---
+++ src/main/java/com/portfolio/system/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.portfolio.system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.portfolio.system.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
